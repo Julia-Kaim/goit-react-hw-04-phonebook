74626a7fdd27192fdced0efcdc62a3420a453a9e
Julia-Kaim <julia.kaim08@gmail.com>
2023-06-13 11:02:43 +0200

    fix: filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import styles from './Filter.module.css';
-
-// const Filter = ({ value, onChange }) => (
-//   <div className={styles.filter}>
-//     <label className={styles.labelFilter}>
-//       Filter
-//       <input
-//         type="name"
-//         value={value}
-//         onChange={onChange}
-//         className={styles.filterInput}
-//       />
-//     </label>
-//   </div>
-// );
-
-// Filter.propTypes = {
-//   value: PropTypes.string.isRequired,
-//   onChangeFilter: PropTypes.func.isRequired,
-// };
-
-// export default Filter;
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Filter.module.css';
